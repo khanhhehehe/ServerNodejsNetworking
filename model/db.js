e5c1a0a14a14c0038fa443853db492f40338f4ce
khanhhehehe <117183565+khanhhehehe@users.noreply.github.com>
2023-08-24 16:03:51 +0700

    first commit

---
+++ model/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/assnetworking')
+    .catch((err) => {
+        console.log(err)
+        console.log('Loi ket noi csdl');
+    })
+module.exports = { mongoose }
